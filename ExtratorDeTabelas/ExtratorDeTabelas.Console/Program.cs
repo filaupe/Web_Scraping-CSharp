4b35c6b9445f483047731c6ce39e841c90b60e4d
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-12-07 19:26:33 -0300

    vou chorar

---
+++ ExtratorDeTabelas/ExtratorDeTabelas.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.RegularExpressions;
+
+var html = File.ReadAllText("C:\\Users\\android\\Documents\\GitHub\\Web_Scraping-CSharp\\ExtratorDeTabelas\\ExtratorDeTabelas.Console\\HTMLScrapping\\exemploTable02.html");
+
+Regex re = new("<tr.*>.*<\\/tr>", RegexOptions.Multiline);
+var table = re.Matches(html);
+foreach (var row in table)
+    Console.WriteLine(row);
