c1597d0d27397ecd609608571d6afc0fa54ef130
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-12-07 21:20:18 -0300

    quase pronto

--- ExtratorDeTabelas/ExtratorDeTabelas.Console/Program.cs
+++ ExtratorDeTabelas/ExtratorDeTabelas.Console/Program.cs
@@ -1,8 +1,30 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
 
-var html = File.ReadAllText("C:\\Users\\android\\Documents\\GitHub\\Web_Scraping-CSharp\\ExtratorDeTabelas\\ExtratorDeTabelas.Console\\HTMLScrapping\\exemploTable02.html");
+var html = File.ReadAllText("C:\\Users\\MulinhaGPlays\\Documents\\GitHub\\Web_Scraping-CSharp\\ExtratorDeTabelas\\ExtratorDeTabelas.Console\\HTMLScrapping\\exemploTable01.html");
 
-Regex re = new("<tr.*>.*<\\/tr>", RegexOptions.Multiline);
-var table = re.Matches(html);
-foreach (var row in table)
-    Console.WriteLine(row);
+Regex reHTML = new("<tr[\\s\\S]*?<\\/tr>", RegexOptions.Multiline);
+var table = reHTML.Matches(html.Replace(" ", ""));
+Regex reTDS = new("<[tdh]*>(.*)</[tdh]*>", RegexOptions.Multiline);
+var tds = reTDS.Matches(table[0].ToString());
+int local = -1;
+List<string> scoreList = new();
+foreach (var td in tds)
+{
+	local++;
+	if (td.ToString()!.ToLower().Contains("score")) break;
+}
+Regex extrairConteudo = new("<td>(.*?)</td>");
+var tableList = table.ToList();
+tableList.RemoveAt(0);
+foreach (var tr in tableList)
+{
+	var trTds = reTDS.Matches(tr.ToString()!);
+	var conteudo = extrairConteudo.Matches(trTds[local].ToString());
+	scoreList.Add(conteudo[0].ToString());
+}
+
+foreach (var item in scoreList)
+{
+	Console.WriteLine(item);
+}
